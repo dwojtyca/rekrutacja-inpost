06383c60c2807c4030b455879ba618028d376042
Dawid Wojtyca <klapek23@gmail.com>
2024-10-03 14:45:07 +0200

    refactor(): use recursion to get categories

--- src/task.ts
+++ src/task.ts
@@ -1,4 +1,4 @@
-import { getCategories } from './mockedApi';
+import { Category, getCategories } from './mockedApi';
 
 export interface CategoryListElement {
   name: string;
@@ -18,70 +18,36 @@ export const categoryTree = async (): Promise<CategoryListElement[]> => {
 
   const toShowOnHome: number[] = [];
 
-  const result = res.data.map((c1) => {
-    let order = c1.Title;
-    if (c1.Title && c1.Title.includes('#')) {
-      order = c1.Title.split('#')[0];
-      toShowOnHome.push(c1.id);
+  const getCategory = (cat: Category) => {
+    let order = cat.Title;
+    if (cat.Title && cat.Title.includes('#')) {
+      order = cat.Title.split('#')[0];
+      toShowOnHome.push(cat.id);
     }
 
-    let orderL1 = parseInt(order);
-    if (isNaN(orderL1)) {
-      orderL1 = c1.id;
+    let orderL = parseInt(order);
+    if (isNaN(orderL)) {
+      orderL = cat.id;
     }
-    const l2Kids = c1.children
-      ? c1.children.map((c2) => {
-          let order2 = c2.Title;
-          if (c2.Title && c2.Title.includes('#')) {
-            order2 = c2.Title.split('#')[0];
-          }
-          let orderL2 = parseInt(order2);
-          if (isNaN(orderL2)) {
-            orderL2 = c2.id;
-          }
-          const l3Kids = c2.children
-            ? c2.children.map((c3) => {
-                let order3 = c3.Title;
-                if (c3.Title && c3.Title.includes('#')) {
-                  order3 = c3.Title.split('#')[0];
-                }
-                let orderL3 = parseInt(order3);
-                if (isNaN(orderL3)) {
-                  orderL3 = c3.id;
-                }
-                return {
-                  id: c3.id,
-                  image: c3.MetaTagDescription,
-                  name: c3.name,
-                  order: orderL3,
-                  children: [],
-                  showOnHome: false,
-                };
-              })
-            : [];
-          l3Kids.sort((a, b) => a.order - b.order);
-          return {
-            id: c2.id,
-            image: c2.MetaTagDescription,
-            name: c2.name,
-            order: orderL2,
-            children: l3Kids,
-            showOnHome: false,
-          };
-        })
-      : [];
-    l2Kids.sort((a, b) => a.order - b.order);
+
     return {
-      id: c1.id,
-      image: c1.MetaTagDescription,
-      name: c1.name,
-      order: orderL1,
-      children: l2Kids,
+      id: cat.id,
+      image: cat.MetaTagDescription,
+      name: cat.name,
+      order: orderL,
+      children: walkTree(cat.children),
       showOnHome: false,
     };
-  });
+  };
 
-  result.sort((a, b) => a.order - b.order);
+  const walkTree = (cats: Category[]) => {
+    if (cats.length === 0) return [];
+    const result = cats.map((cat) => getCategory(cat));
+    result.sort((a, b) => a.order - b.order);
+    return result;
+  };
+
+  const result = walkTree(res.data);
 
   if (result.length <= 5) {
     result.forEach((a) => (a.showOnHome = true));
